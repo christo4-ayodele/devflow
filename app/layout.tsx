e92d40333b9f9e8949b7c0fca6e57afe548a1a21
trashyfish <ayodelebabajidechris123@gmail.com>
2024-07-16 08:43:17 +0100

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,50 @@
 import { ClerkProvider } from '@clerk/nextjs';
-import './globals.css';
+
 import React from 'react';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
+
+export const metadata: Metadata = {
+  title: 'Devflow',
+  description:
+    'A Community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developerd from around the world. Explore the topics in web development, mobile app development, algorithms, data structure, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+      afterSignOutUrl="/"
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
