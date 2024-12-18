8b597131f784ea2e2842832b0e2996b6f0d2d6c8
Jide <ayodelebabajidechris123@gmail.com>
2024-12-18 13:44:22 +0100

    Tailwind CSS setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-violet-700">
+    <h1 className="text-3xl font-black text-primary-500">
       Welcome to the world of Nextjs
     </h1>
   );
