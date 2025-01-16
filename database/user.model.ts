279f910b65249ea1d9d0444c34334643adf23f05
Jide <ayodelebabajidechris123@gmail.com>
2025-01-16 01:36:13 +0100

    Create User Model

---
+++ database/user.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema } from "mongoose";
+import { StringValidation } from "zod";
+
+export interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String, required: false },
+    image: { type: String, required: false },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
