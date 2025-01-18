4459522b2a1517e3d7f6681d0f13ebd06505ad64
Jide <ayodelebabajidechris123@gmail.com>
2025-01-18 06:45:09 +0100

    Implement Answer Model

--- database/question.model.ts
+++ database/question.model.ts
@@ -11,16 +11,19 @@ export interface IQuestion {
   author: Types.ObjectId;
 }
 
-const QuestionSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
-  views: { type: Number, default: 0 },
-  upvotes: { type: Number, default: 0 },
-  downvotes: { type: Number, default: 0 },
-  answers: { type: Number, default: 0 },
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
 const Question =
   models?.question || model<IQuestion>("Question", QuestionSchema);
