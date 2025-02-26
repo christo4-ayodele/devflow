19851c64cee6c57fafb20756fe7f1e09bffbd5d5
Jide <ayodelebabajidechris123@gmail.com>
2025-02-26 04:00:11 +0100

    Question Views (Approach 3.1)

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -7,6 +7,7 @@ import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 
 // const sampleQuestion = {
 //   id: "q123",
@@ -78,11 +79,11 @@ import { redirect } from "next/navigation";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
+  const { success, data: question } = await getQuestion({ questionId: id });
 
-  const [_, { success, data: question }] = await Promise.all([
-    await incrementViews({ questionId: id }),
-    await getQuestion({ questionId: id }),
-  ]);
+  after(async () => {
+    await incrementViews({ questionId: id });
+  });
 
   if (!success || !question) return redirect("/404");
 
